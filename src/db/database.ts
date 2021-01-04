b8b034d92ccdac76b569b1c74accbd6754a1d27e
patdev9 <patrick.lungu@live.fr>
2021-01-04 15:03:52 +0100

    modife env

--- src/db/database.ts
+++ src/db/database.ts
@@ -3,4 +3,4 @@ import { config } from "dotenv";
 config()
 console.log(process.env.DB_SEQ)
 
-export const sequelize = new Sequelize('mysql://root:root@localhost:8889/Type')
+export const sequelize = new Sequelize(`${process.env.DB_SEQ}`)
