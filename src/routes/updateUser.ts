a90a86d5c3454f471e6ed881d1e74c5391ea8180
juanitoo28 <jean28tiago@gmail.com>
2021-01-06 20:43:44 +0100

    test

---
+++ src/routes/updateUser.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+
+import {registerMidd, loginMidd, authMidd } from '../middlewares/auth.middleware';
+import { AuthController } from '../controller/auth';
+import CartController from '../controller/cart';
+
+
+
+const route: Router = Router();
+
+
+route.get('/updateUser', authMidd, (req: any, res: any) => {
+    return res.end('<h1>Modification User</h1>')
+})
+route.post('/login', loginMidd, AuthController.login)
+route.post('/register', registerMidd, AuthController.register)
+route.post('/cart',authMidd, CartController.addCart)
+
+
+export { route as AuthentificationRoute }
